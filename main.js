8871e8b1c27a164895e8429f7696992ed435d7c6
maurotpolo <78087588+maurotpolo@users.noreply.github.com>
2022-04-26 22:21:42 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
- consoleText(['@maurotpolo...', 'font: Roboto...', 'Made with Love.'], 'text',['tomato','rebeccapurple','lightblue']);
+ consoleText(['@maurotpolo', 'Made with Love.'], 'text',['DarkSlateGrey','lightblue']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
